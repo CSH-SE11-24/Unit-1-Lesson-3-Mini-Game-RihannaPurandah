2984204db816df3ab96eccd727d4585d447b75f4
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:26:19 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Get started!
+
+let name = prompt("What is your name?")
+let age = prompt("What is your age")
+
+age = parseInt(age)
+
+let catergory = prompt("Which one do you wanna be? A mage, healer, fighter, tank, assassin?")
+
+const question = prompt("Are you sure?")
+console.log(`${question},  Thank you for answering`)
+
+const welcome = "Welcome to Solo Leveling"
+console.log(welcome)
+
+let powerLevel1 = "D"
+let powerLevel2 = "C"
+let powerLevel3 = "B"
+let powerLevel4 = "A"
+let powerLevel5 = "S"
+let powerLevel6 = "SS"
+let powerLevel7 = "SSS"
+let powerLevel8 = "U"
